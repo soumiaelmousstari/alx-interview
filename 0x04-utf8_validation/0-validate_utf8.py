f4d9a2facbe4eb8959841d006102d2d6d5a2f699
SOUMIA_EL_MOUSSTARI <elmousstarisoumia@gmail.com>
2024-08-21 12:46:00 +0000

    0. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,18 +3,20 @@
 
 
 def validUTF8(data):
+    """This function determines if a given data
+    set represents a valid UTF-8 encoding."""
     c = 0
     for b in data:
         d = bin(b).replace('0b', '').rjust(8, '0')[-8:]
         if c == 0:
-           if d.startswith('110'):
+            if d.startswith('110'):
                 c = 1
             if d.startswith('1110'):
                 c = 2
             if d.startswith('11110'):
                 c = 3
             if d.startswith('10'):
-               return False
+                return False
         else:
             if not d.startswith('10'):
                 return False
